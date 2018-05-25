09b075772541bb9534b1109315211c31b72873f0
naru014 <narayanan.kollengoderanganathan@capitalone.com>
2018-05-25 14:14:47 +0530

    Adding Files

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var requests=0;
+var podname= process.env.HOSTNAME;
+var startTime;
+var host;
+var handleRequest = function(request, response) {
+  response.setHeader('Content-Type', 'text/plain');
+  response.writeHead(200);
+  response.write("!!!!!!!!!!!!!!!!!Welcome to Kubernetes Test Page!!!!!!!!!!!!!!!!!\n");
+  response.write("This page give you information about where the current request is routed to!!!!\n");
+  response.write("The request is served by: ");
+  response.write(host);
+  response.end("\nThank you for visting this page!!!!!!!!!!\n");
+  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
+}
+var www = http.createServer(handleRequest);
+www.listen(8080,function () {
+    startTime = new Date();;
+    host = process.env.HOSTNAME;
+    console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
+});
